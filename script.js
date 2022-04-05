5c5be2b30bcdfef419e3d8865ab090557a21a3a2
Shafoat <85842043+kadsyo-00@users.noreply.github.com>
2022-04-05 17:24:48 +0500

    JS

---
+++ script.js
@@ -0,0 +1,19 @@
+let m = +prompt('455-125=?')
+console.log(m);
+console.log('455-125 = 330');
+
+let i = +prompt('45 * 5 = ?')
+console.log(i);
+console.log('45*5=225');
+
+let s = +prompt('860/10=?')
+console.log(s);
+console.log('860/10 = 86');
+
+let o = +prompt('555+205=?')
+console.log(o);
+console.log('555+205 = 660');
+
+let l = +prompt('450+60=?')
+console.log(l);
+console.log('450+60 = 510');
